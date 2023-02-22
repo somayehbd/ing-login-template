43958f9b31ef449e78b1132a13dec15c877b31a1
somayehbd <somayehbd@gmail.com>
2023-02-22 22:26:07 +0100

    handled form submit event

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,12 +1,21 @@
-var requestOptions = {
-    method: 'POST',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({
-        "username": "demo",
-        "password": "123456"
-    }),
-    redirect: 'follow'
-};
-fetch('http://localhost:3100/login', requestOptions)
-    .then(response => { console.log(response) })
+document.getElementById('submit').addEventListener('click', (e) => {
+    e.preventDefault();
 
+    var username = document.getElementById('username');
+    var password = document.getElementById('pw');
+    
+    var requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            "username": username.value,
+            "password": password.value
+        }),
+        redirect: 'follow'
+    };
+
+    fetch('http://localhost:3100/login', requestOptions)
+        .then(response => response.json())
+        .then(response => { alert('Logged in successfully') })
+        .catch(() => { alert('Log in failed') })
+});
