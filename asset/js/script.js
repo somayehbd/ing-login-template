c58d253f9bd94c3136e8e60881ba38a8fa7cda29
somayehbd <somayehbd@gmail.com>
2023-02-23 23:31:25 +0100

    improved login method

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,24 +1,3 @@
-// document.getElementById('submit').addEventListener('click', (e) => {
-//     e.preventDefault();
-
-//     var username = document.getElementById('username');
-//     var password = document.getElementById('pw');
-
-//     var requestOptions = {
-//         method: 'POST',
-//         headers: { 'Content-Type': 'application/json' },
-//         body: JSON.stringify({
-//             "username": username.value,
-//             "password": password.value
-//         }),
-//         redirect: 'follow'
-//     };
-
-//     fetch('http://localhost:3100/login', requestOptions)
-//         .then(response => response.json())
-//         .then(response => { alert('Logged in successfully') })
-//         .catch(() => { alert('Log in failed') })
-// });
 const form = document.getElementById('form')
 const username = document.getElementById('username')
 const password = document.getElementById('pw')
@@ -48,14 +27,10 @@ const handleform = (e) => {
         })
         .then(data => {
             console.log(data)
-            if (username.value == "demo" && password.value == "123456") {
-                alert(`${username.value}, ${data.token}`)
-            }
-
+            alert(`${username.value}, ${data.token}`)
         })
-        .catch(() => { console.log('incorrect password') })
-
-
+        .catch(() => { alert('incorrect username or password') })
 }
+
 form.addEventListener('submit', handleform)
 
