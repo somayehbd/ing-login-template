00947a0b71036027e76e2b7e798f2db08623b296
somayehbd <somayehbd@gmail.com>
2023-02-26 15:47:12 +0100

    feat: handled business form

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,18 +1,19 @@
-const handleform = (e) => {
+const personalForm = document.getElementById('personalForm')
+
+const handlePersonalForm = (e) => {
     e.preventDefault()
 
-    const form = document.getElementById('personalForm')
-    const username = form.username
-    const password = form.password
+    const username = personalForm.username
+    const password = personalForm.password
 
     myHeaders = { "Content-Type": "application/json" };
 
-    var raw = JSON.stringify({
+    let raw = JSON.stringify({
         "username": username.value,
         "password": password.value
     });
 
-    var requestOptions = {
+    let requestOptions = {
         method: 'POST',
         headers: myHeaders,
         body: raw,
@@ -21,15 +22,62 @@ const handleform = (e) => {
 
     fetch('http://localhost:3100/personal/login', requestOptions)
         .then(response => {
-            console.log(response);
+            // console.log(response);
             return response.json();
         })
         .then(data => {
-            console.log(data)
-            alert(`${username.value}, ${data.token}`)
+            // console.log(data)
+            if (data.success)
+                alert(`${username.value}, ${data.token}`)
+            else
+                alert('username or password is incorrect [personal service]')
         })
-        .catch(() => { alert('incorrect username or password') })
+        .catch(() => { alert('error') })
 }
 
-form.addEventListener('submit', handleform)
+personalForm.addEventListener('submit', handlePersonalForm)
+
+// handleBusinessForm
+const businessForm = document.getElementById('businessForm')
+
+const handlebusinessForm = (e) => {
+    e.preventDefault();
+
+    const username = businessForm.username;
+    const password = businessForm.password;
+
+    myHeaders = { "Content-Type": "application/json" };
+
+    let raw = JSON.stringify({
+        "username": username.value,
+        "password": password.value
+    });
+
+    let requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch('http://localhost:3100/business/login', requestOptions)
+        .then(response => {
+            // console.log(respo/nse)
+            return response.json()
+        })
+        .then(data => {
+            // console.log(data)
+            if (data.success == true) {
+                alert(`${username.value}, ${data.token}`)
+            }
+            else {
+                alert('username or password is incorrect')
+            }
+        })
+        .catch(() => { alert('error') })
+
+
+}
+
+businessForm.addEventListener('submit', handlebusinessForm)
 
